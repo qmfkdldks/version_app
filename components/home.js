a08a7d1e6cc34000b4de17d83c1987cc6eab1a5a
Brian Kang <qmfkdldks@gmail.com>
2019-07-16 13:17:35 -0300

    feat: added my cupons page

---
+++ components/home.js
@@ -0,0 +1,22 @@
+// import * as React from 'react';
+// import { StyleSheet } from 'react-native';
+// import { Button, Layout, Text, List, ListItem } from 'react-native-ui-kitten';
+// import {TopNavigationShowcase} from './top_navigation';
+
+// export const HomeScreen = () => (
+//   <Layout style={styles.container}>
+//     <TopNavigationShowcase />
+//     <Text style={styles.text} category='h4'>Welcome to UI Kitten</Text>
+//     <Button>BUTTON</Button>
+//   </Layout>
+// );
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//   },
+//   text: {
+//     marginVertical: 16,
+//   },
+// });
